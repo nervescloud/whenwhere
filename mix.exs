625b1ea0983faf5cbabb54518fd02e3b9fb33970
Lars Wikman <lars@underjord.io>
2024-07-15 11:27:23 +0200

    Update to v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Whenwhere.MixProject do
   def project do
     [
       app: :whenwhere,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
